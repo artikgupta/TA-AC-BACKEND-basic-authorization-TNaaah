fbc0a6b44ca50e60196147bc84b031aa1cd2b4de
artikgupta <artikgupta>
2021-05-22 02:34:50 +0530

    completed assignment III

---
+++ block-BNaaeb/podcast/middlewares/auth.js
@@ -0,0 +1,43 @@
+var User = require("../models/User")
+
+var Podcast = require("../models/Podcast")
+
+module.exports={
+    loggedInUser:(req,res,next)=>{
+        if(req.session && req.session.userId){
+            next()
+        }else{
+            res.redirect("/users/login")
+        }
+    },
+
+    userInfo:(req,res, next)=>{
+   var userId = req.session && req.session.userId
+   if(userId){
+       User.findById(userId,' name email isAdmin category', (err, user)=>{
+        if(err) return next(err)
+        req.user = user;
+        res.locals.user = user;
+        next();
+      });
+    } else {
+      req.user = null;
+      res.locals.user = null;
+      next();
+    }
+    },
+  adminUser:(req,res, next)=>{
+   var id = req.session.userId;
+   console.log(id)
+   User.findById(id,(err, user) =>{
+       console.log(user)
+       if(err || !user) {
+       return next("User not found")
+        } else if(user.isAdmin) {
+          return next()
+        }else{
+          return res.render("noauth")
+       }
+   })}
+
+}
